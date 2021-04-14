f019aeef53699ec8663a2c282fed0a8d7268fff8
Ziniu Lu <luziniuoskar@outlook.com>
2021-04-14 15:24:35 +0200

    declaration of all included libigl headers.

---
+++ iglHeader.h
@@ -0,0 +1,10 @@
+/* Created on: 03/04/2021
+ *     Author: Ziniu Lu (luziniuoskar@outlook.com)
+ */
+
+#pragma once
+
+#include <igl/list_to_matrix.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <Eigen/Dense>
+
