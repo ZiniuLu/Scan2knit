3c01f5b80481b57c2fef58cfa31289031847d449
Ziniu Lu <luziniuoskar@outlook.com>
2021-05-03 02:44:53 +0200

    add new APIs to display mesh / skeleton via libigl

--- Display.cpp
+++ Display.cpp
@@ -2,13 +2,184 @@
  *     Author: Ziniu Lu (luziniuoskar@outlook.com)
  */
 
-
-
 #include "Display.h"
+#include "Skeletonization.h"
 
-//BEGIN_PROJECT_NAMESPACE
+BEGIN_PROJECT_NAMESPACE
+
+// Display::
+Display::Display() { }
+Display::Display(const std::string path) { this->display(path); }
+Display::Display(const MAT_3d& V, const MAT_3i& F) { this->display(V, F); }
+Display::Display(const MAT_3d& V, const MAT_2i& E) { this->display(V, E); }
+Display::Display(const Polyhedron& tmesh) { this->display_tmesh(tmesh); }
+Display::Display(const Skeleton& skeleton) { this->display_skeleton(skeleton); }
+Display::Display(const Skel& skel) { this->display_skel(skel); }
+Display::Display(const SkelGraph& skel_graph) { this->display_skel_graph(skel_graph); }
+
+// public
+void Display::display(const std::string path)
+{
+    if (path.find(".off") > 0)
+    {
+        this->display_off(path);
+    }
+    else if (path.find(".obj") > 0)
+    {
+        this->display_obj(path);
+    }
+    else if (path.find(".stl") > 0)
+    {
+        this->display_stl(path);
+    }
+}
+void Display::display(const MAT_3d& V, const MAT_3i& F)
+{
+    int numV = V.rows();
+    int numF = F.rows();
+
+    // set para for current viewer
+    this->viewer.data().point_size = V_SIZE;
+    this->viewer.data().line_width = E_SIZE;
+    this->viewer.core().camera_zoom = CAM_ZOOM;
+
+    // lauch current viewer
+    this->viewer.data().set_mesh(V, F);
+    this->viewer.launch();
+
+
+}
+void Display::display(const MAT_3d& V, const MAT_2i& E)
+{
+    int numV = V.rows();
+    int numE = E.rows();
+
+    // set para for current viewer
+    this->viewer.data().point_size = V_SIZE;
+    this->viewer.data().line_width = E_SIZE;
+
+    // add points
+    auto color_v = Eigen::RowVector3d(0, 1, 0);
+    this->viewer.data().add_points(V, color_v);
+
+    // add edges
+    for (unsigned i = 0; i < numE; ++i)
+    {
+        int index1 = E(i, 0) - 1;   // source vertex number
+        int index2 = E(i, 1) - 1;   // target vertex number
+
+        auto& source = V.row(index1);
+        auto& target = V.row(index2);
+        auto color_eg = Eigen::RowVector3d(1, 0, 0);
+
+        this->viewer.data().add_edges(source, target, color_eg);
+        this->viewer.launch();
+    }
+    this->viewer.core().camera_zoom = CAM_ZOOM;
+    this->viewer.launch();
+}
+void Display::display(const Polyhedron& tmesh)
+{
+    this->display_tmesh(tmesh);
+}
+void Display::display(const Skeleton& skeleton)
+{
+    this->display_skeleton(skeleton);
+}
+void Display::display(const Skel& skel)
+{
+    this->display_skel(skel);
+}
+void Display::display(const SkelGraph& skel_graph)
+{
+    this->display_skel_graph(skel_graph);
+}
 
 
 
-//END_PROJECT_NAMESPACE
+
+// private
+
+void Display::display_tmesh(const Polyhedron& tmesh) 
+{
+    auto it_v_begin = tmesh.vertices_begin();
+    auto it_v_end = tmesh.vertices_end();
+    auto it_v = it_v_begin;
+
+    for (; it_v != it_v_end; ++it_v)
+    {
+        const Point& p = (*it_v).point();
+        this->V << p.x(), p.y(), p.z();
+    }
+
+    auto it_f_begin = tmesh.facets_begin();
+    auto it_f_end = tmesh.facets_end();
+    auto it_f = it_f_begin;
+
+    for (; it_f != it_f_end; ++it_f)
+    {
+        auto f_0 = it_f->facet_begin();
+        auto f_1 = f_0.
+
+        this->F << *it_f;
+    }
+}
+
+void Display::display_obj(const std::string path)
+{
+
+}
+
+void Display::display_off(const std::string path)
+{
+    igl::readOFF(path, this->V, this->F);
+    this->viewer.data().set_mesh(this->V, this->F);
+    this->viewer.launch();
+}
+void Display::display_stl(const std::string path)
+{
+    std::ifstream input(path);
+    igl::readSTL(input, this->V, this->F, this->N);
+
+    this->viewer.data().set_mesh(this->V, this->F);
+    this->viewer.launch();
+
+}
+
+void Display::display_skeleton(const Skeleton& skeleton)
+{
+    Skel skel(skeleton);
+    this->display_skel(skel);
+
+}
+void Display::display_skel(const Skel& skel)
+{
+    SkelGraph skel_graph(skel);
+    this->display_skel_graph(skel_graph);
+}
+void Display::display_skel_graph(const SkelGraph& skel_graph)
+{
+    const std::vector<SkelNode>& skel_nodes = skel_graph.get_skel_nodes();
+    const std::vector<SkelEdge>& skel_edges = skel_graph.get_skel_edges();
+
+    size_t numV = skel_nodes.size();
+    size_t numE = skel_edges.size();
+
+    //this->V.resize(numV, 3);
+    //this->E.resize(numE, 2);
+
+    for (SkelNode nd : skel_nodes)
+    {
+        const Point& p = nd.point();
+        this->V << p.x(), p.y(), p.z();
+    }
+    for (SkelEdge eg : skel_edges)
+    {
+        this->E << eg.source() - 1, eg.target() - 1;
+    }
+
+    this->display(V, E);
+}
+
+END_PROJECT_NAMESPACE
 
--- Display.h
+++ Display.h
@@ -4,29 +4,56 @@
 
 #pragma once
 
+#include <string>
+
 #include "project.h"
+//#include "cgalHeader.h"
 #include "iglHeader.h"
+#include "Skeletonization.h"
 
 
 #define V_SIZE 5
 #define E_SIZE 3.0f
-
-
+#define CAM_ZOOM 0.05;
 
 BEGIN_PROJECT_NAMESPACE
 
 class Display
 {
 private:
-	size_t V_len = 0;
-	size_t E_len = 0;
-	Eigen::MatrixXd V_cache;
-	Eigen::MatrixXi E_cache;
-public:
+	MAT_3d	   V;
+	MAT_2i	   E;
+	MAT_3i	   F;
+	MAT_3d	   N;	//face normal
+	IGL_Viewer viewer;
 
-	void display_once(
-		const Eigen::MatrixXd& V,
-		const Eigen::MatrixXi& E);
+	void display_tmesh(const Polyhedron& tmesh);
+	void display_skeleton(const Skeleton& skeleton);
+	void display_skel(const Skel& skel);
+	void display_skel_graph(const SkelGraph& skel_graph);
+
+
+public:
+	Display();
+	Display(const std::string path);
+	Display(const MAT_3d& V, const MAT_3i& F);
+	Display(const MAT_3d& V, const MAT_2i& E);
+	Display(const Polyhedron& tmesh);
+	Display(const Skeleton& skeleton);
+	Display(const Skel& skel);
+	Display(const SkelGraph& skel_graph);
+
+	void display(const std::string path);
+	void display(const Polyhedron& tmesh);// display triangle mesh
+	void display(const Skeleton& skeleton);
+	void display(const Skel& skel);
+	void display(const SkelGraph& skel_graph);
+	void display(const MAT_3d& V, const MAT_3i& F);
+	void display(const MAT_3d& V, const MAT_2i& E);
+
+	void display_obj(const std::string path);
+	void display_off(const std::string path);
+	void display_stl(const std::string path);
 };
 
 END_PROJECT_NAMESPACE
