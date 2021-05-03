1922d5945a6ea7f87896cb2ee21752c8d442511d
Ziniu Lu <luziniuoskar@outlook.com>
2021-05-03 02:41:35 +0200

    simplify class name

--- iglHeader.h
+++ iglHeader.h
@@ -8,3 +8,9 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <Eigen/Dense>
 
+typedef Eigen::Matrix<double, Eigen::Dynamic, 3> MAT_3d;
+typedef Eigen::Matrix<int, Eigen::Dynamic, 3>	 MAT_3i;
+typedef Eigen::Matrix<int, Eigen::Dynamic, 2>	 MAT_2i;
+typedef igl::opengl::glfw::Viewer				 IGL_Viewer;
+
+
