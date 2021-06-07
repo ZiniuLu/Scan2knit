5df6ac5928188cdb0a269098fe07b3cc12f83adc
Ziniu Lu <luziniuoskar@outlook.com>
2021-06-07 09:37:48 +0200

    using new class "GuiConsole" to print program Info in GUI

--- project.h
+++ project.h
@@ -3,7 +3,11 @@
  */
 
 #pragma once
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <imgui.h>
 
 # define PROJECT_NAMESPACE Scan2knit
 # define BEGIN_PROJECT_NAMESPACE namespace PROJECT_NAMESPACE {
@@ -11,39 +15,41 @@
 
 BEGIN_PROJECT_NAMESPACE
 
-struct MESH_FILE
+struct GuiConsole
 {
-	std::string folder_path;
-	std::string name;
+	char                  InputBuf[256];
+	ImVector<char*>       Items;
+	ImVector<const char*> Commands;
+	ImVector<char*>       History;
+	int                   HistoryPos;    // -1: new line, 0..History.Size-1 browsing history.
+	ImGuiTextFilter       Filter;
+	bool                  AutoScroll;
+	bool                  ScrollToBottom;
+
+	GuiConsole();
+	~GuiConsole();
+
+	// Portable helpers
+	static int   Stricmp(const char* s1, const char* s2);
+	static int   Strnicmp(const char* s1, const char* s2, int n);
+	static char* Strdup(const char* s);
+	static void  Strtrim(char* s);
+
+	void         ClearLog();
+	void         AddLog(const char* fmt, ...);
+	void         Draw(const char* title, bool* p_open);
+	void         ExecCommand(const char* command_line);
+
+	static int   TextEditCallbackStub(ImGuiInputTextCallbackData* data);
+	int          TextEditCallback(ImGuiInputTextCallbackData* data);
 };
 
-struct STITCH
-{
-	double obj_scale;
-	double stitch_width;
-	double stitch_height;
-	double layer0_offset;
-};
-
-struct VIEWER
-{
-	double point_size;
-	double line_width;
-	double camera_zoom;
-};
-
-struct Settings
-{
-	bool		loaded;
-	std::string root_path;	// root directory of current solution
-
-	MESH_FILE   File;	// file name of 3d mesh, read from Settings.csv
-	STITCH		Stitch;
-	VIEWER      Viewer;
-
-	void load();
-	void print();
-};
-
+//class Console
+//{
+//public:
+//	
+//};
 
 END_PROJECT_NAMESPACE
+
+void Print(const std::string& text);
